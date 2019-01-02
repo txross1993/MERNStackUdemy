ce1e150aa8f7e0758592b4d172c3e1c8ddf68135
txross1993 <thea.ross@yahoo.com>
2019-01-02 17:01:55 -0600

    section 2 lecture 8

---
+++ README.md
@@ -0,0 +1,1 @@
+https://tysonfoodsit.udemy.com/practical-angular-react-node-airbnb-style-application/learn/v4/t/lecture/10820274?start=300
---
+++ server.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const users = require('./routes/api/Users');
+const profile = require('./routes/api/Profiles');
+const posts = require('./routes/api/Posts');
+
+const app = express();
+
+// DB Config
+const db = require('./config/keys').mongoURI;
+
+// Connect to MongoDB
+mongoose
+  .connect(db)
+  .then(() => {
+    console.log('MongoDb connected');
+  })
+  .catch((err) => console.log(err));
+
+app.get('/', (req, res) => {
+  res.send('Hello');
+});
+
+//Use routes
+app.use('/api/users', users);
+app.use('/api/posts', posts);
+app.use('/api/profile', profile);
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
