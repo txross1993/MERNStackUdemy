6884871dd12b866d436003c8fbff4d5c0f4342e2
Theadora Ross <thea.ross@yahoo.com>
2019-01-03 23:34:35 -0600

    created User model - section 3, video 9

---
+++ models/User.js
@@ -0,0 +1,30 @@
+// The convention for models is to be singular and Capitalized
+
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+// Create schema
+const UserSchema = new Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  avatar: {
+    type: String,
+    required: true
+  },
+  date: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = User = mongoose.model("users", UserSchema);
